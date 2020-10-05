5597671c1d6dc190da1f45a8932d9848c15b31cb
vaughn <vaughncato@gmail.com>
2020-10-04 20:45:40 -0400

    Got a working imgui example that works with wasm

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -1,49 +1,157 @@
+#include <iostream>
 #include <Magnum/GL/DefaultFramebuffer.h>
-#include <Magnum/GL/Renderer.h>
-#include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/Math/Color.h>
-
-#define ADD_IMGUI 1
-
-#if ADD_IMGUI
+#include <Magnum/GL/Renderer.h>
 #include <Magnum/ImGuiIntegration/Context.hpp>
+
+#ifdef CORRADE_TARGET_ANDROID
+#include <Magnum/Platform/AndroidApplication.h>
+#elif defined(CORRADE_TARGET_EMSCRIPTEN)
+#include <Magnum/Platform/EmscriptenApplication.h>
+#else
+#include <Magnum/Platform/Sdl2Application.h>
 #endif
 
-using namespace Magnum;
+namespace Math = Magnum::Math;
+namespace Platform = Magnum::Platform;
+namespace ImGuiIntegration = Magnum::ImGuiIntegration;
+namespace GL = Magnum::GL;
+using Color4 = Magnum::Color4;
+using Float = Magnum::Float;
+using Double = Magnum::Double;
+using Magnum::NoCreate;
+using Magnum::Vector2;
+using namespace Math::Literals;
 
 
-class MyApplication : public Platform::Application
+class MyApplication: public Platform::Application
 {
-  public:
-    explicit MyApplication(const Arguments& arguments);
+    public:
+        explicit MyApplication(const Arguments& arguments);
 
-  private:
-#if ADD_IMGUI
-    ImGuiIntegration::Context _imgui{NoCreate};
-#endif
-    void drawEvent() override;
+        void drawEvent() override;
+
+        void viewportEvent(ViewportEvent& event) override;
+
+        void keyPressEvent(KeyEvent& event) override;
+        void keyReleaseEvent(KeyEvent& event) override;
+
+        void mousePressEvent(MouseEvent& event) override;
+        void mouseReleaseEvent(MouseEvent& event) override;
+        void mouseMoveEvent(MouseMoveEvent& event) override;
+        void mouseScrollEvent(MouseScrollEvent& event) override;
+        void textInputEvent(TextInputEvent& event) override;
+
+    private:
+        ImGuiIntegration::Context _imgui{NoCreate};
+
+        //bool _showDemoWindow = true;
+        bool _showAnotherWindow = false;
+        Color4 _clearColor = 0x72909aff_rgbaf;
+        //Float _floatValue = 0.0f;
 };
 
 
 MyApplication::MyApplication(const Arguments& arguments)
-  : Platform::Application{arguments}
+: Platform::Application{
+    arguments,
+    Configuration{}
+      .setTitle("Magnum ImGui Example")
+      .setWindowFlags(Configuration::WindowFlag::Resizable)
+  }
 {
-  /* TODO: Add your initialization code here */
-  using namespace Math::Literals;
-  GL::Renderer::setClearColor(0xa5c9ea_rgbf);
-#if ADD_IMGUI
-  _imgui.newFrame();
+    _imgui = ImGuiIntegration::Context(Vector2{windowSize()}/dpiScaling(),
+        windowSize(), framebufferSize());
+
+    /* Set up proper blending to be used by ImGui. There's a great chance
+       you'll need this exact behavior for the rest of your scene. If not, set
+       this only for the drawFrame() call. */
+    GL::Renderer::setBlendEquation(GL::Renderer::BlendEquation::Add,
+        GL::Renderer::BlendEquation::Add);
+    GL::Renderer::setBlendFunction(GL::Renderer::BlendFunction::SourceAlpha,
+        GL::Renderer::BlendFunction::OneMinusSourceAlpha);
+
+#if !defined(MAGNUM_TARGET_WEBGL) && !defined(CORRADE_TARGET_ANDROID)
+    /* Have some sane speed, please */
+    setMinimalLoopPeriod(16);
 #endif
 }
 
+void MyApplication::drawEvent() {
+    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
 
-void MyApplication::drawEvent()
-{
-  GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+    _imgui.newFrame();
 
-  /* TODO: Add your drawing code here */
+    /* Enable text input, if needed */
+    if(ImGui::GetIO().WantTextInput && !isTextInputActive())
+        startTextInput();
+    else if(!ImGui::GetIO().WantTextInput && isTextInputActive())
+        stopTextInput();
 
-  swapBuffers();
+    ImGui::Begin("Another Window", &_showAnotherWindow);
+    ImGui::Text("Hello, world!");
+    ImGui::End();
+
+    /* Update application cursor */
+    _imgui.updateApplicationCursor(*this);
+
+    /* Set appropriate states. If you only draw ImGui, it is sufficient to
+       just enable blending and scissor test in the constructor. */
+    GL::Renderer::enable(GL::Renderer::Feature::Blending);
+    GL::Renderer::enable(GL::Renderer::Feature::ScissorTest);
+    GL::Renderer::disable(GL::Renderer::Feature::FaceCulling);
+    GL::Renderer::disable(GL::Renderer::Feature::DepthTest);
+
+    _imgui.drawFrame();
+
+    /* Reset state. Only needed if you want to draw something else with
+       different state after. */
+    GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+    GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+    GL::Renderer::disable(GL::Renderer::Feature::ScissorTest);
+    GL::Renderer::disable(GL::Renderer::Feature::Blending);
+
+    swapBuffers();
+    redraw();
+}
+
+void MyApplication::viewportEvent(ViewportEvent& event) {
+    GL::defaultFramebuffer.setViewport({{}, event.framebufferSize()});
+
+    _imgui.relayout(Vector2{event.windowSize()}/event.dpiScaling(),
+        event.windowSize(), event.framebufferSize());
+}
+
+void MyApplication::keyPressEvent(KeyEvent& event) {
+    if(_imgui.handleKeyPressEvent(event)) return;
+}
+
+void MyApplication::keyReleaseEvent(KeyEvent& event) {
+    if(_imgui.handleKeyReleaseEvent(event)) return;
+}
+
+void MyApplication::mousePressEvent(MouseEvent& event) {
+    if(_imgui.handleMousePressEvent(event)) return;
+}
+
+void MyApplication::mouseReleaseEvent(MouseEvent& event) {
+    if(_imgui.handleMouseReleaseEvent(event)) return;
+}
+
+void MyApplication::mouseMoveEvent(MouseMoveEvent& event) {
+    if(_imgui.handleMouseMoveEvent(event)) return;
+}
+
+void MyApplication::mouseScrollEvent(MouseScrollEvent& event) {
+    if(_imgui.handleMouseScrollEvent(event)) {
+        /* Prevent scrolling the page */
+        event.setAccepted();
+        return;
+    }
+}
+
+void MyApplication::textInputEvent(TextInputEvent& event) {
+    if(_imgui.handleTextInputEvent(event)) return;
 }
 
 
